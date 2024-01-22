f007a1d2e58c4bcf7e2172041fb68aae04801e99
Apollo3zehn <git@m1.apollo3zehn.net>
2024-01-22 22:45:10 +0100

    Try to parse files

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,96 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Uri, window, workspace } from 'vscode';
+import { posix } from 'path';
+
+const logger = window.createOutputChannel('Cortex Notes');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cortex-notes" is now active!');
+    logger.appendLine('Extension activated');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cortex-notes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cortex-notes!');
-	});
+    if (workspace.workspaceFolders === undefined) {
+        logger.appendLine('No workspace open, exiting');
+        return;
+    }
 
-	context.subscriptions.push(disposable);
+    const fileUris = await workspace.findFiles("**/*.md");
+    const cortex = new Map<string, FileMetadata>();
+    const linkRegex = /\[{2}(.*?)\]{2}/gm;
+
+    for (const fileUri of fileUris) {
+
+        if (fileUri.scheme !== 'file')
+            continue;
+
+        logger.appendLine(`Processing ${fileUri.toString()}`);
+
+        const document = await workspace.openTextDocument(fileUri);
+        const text = document.getText();
+
+        const sourcePageName = getPageName(fileUri);
+        const metadataSource = GetOrCreateMetadata(cortex, sourcePageName);
+        let match: RegExpExecArray | null;
+
+        while ((match = linkRegex.exec(text))) {
+            logger.appendLine(`Found link ${match[1]}`);
+
+            const targetPageName = match[1];
+            const metadataTarget = GetOrCreateMetadata(cortex, targetPageName);
+        }
+    }
+
+    let disposable = vscode.commands.registerCommand('cortex-notes.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from cortex-notes!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+function getPageName(uri: Uri): string {
+    return changeExtension(posix.basename(uri.fsPath), '*', '')
+}
+
+// TODO replace by own implementation
+function changeExtension(
+    path: string,
+    from: string,
+    next: string
+): string {
+    const current = posix.extname(path);
+
+    if ((from === '*' && current !== next) || current === from) {
+        path = path.substring(0, path.length - current.length);
+        return next ? path + next : path;
+    }
+
+    return path;
+}
+
+function GetOrCreateMetadata(cortex: Map<string, FileMetadata>, pageName: string, ) {
+    let fileMetadata: FileMetadata;
+
+    if (cortex.has(pageName)) {
+        fileMetadata = cortex.get(pageName)!;
+    }
+
+    else {
+        logger.appendLine(`Create page ${pageName}`);
+        fileMetadata = new FileMetadata([], []);
+        cortex.set(pageName, fileMetadata);
+    }
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+class FileMetadata {
+    constructor(
+        public readonly links: string[],
+        public readonly backlinks: string[]) {
+        //
+    }
+}
